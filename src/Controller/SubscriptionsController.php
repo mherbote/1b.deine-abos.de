36215e53c4afea3d4d1c53ffff14599dd719941c
mherbote <mherbote@t-online.de>
2023-08-16 18:03:13 +0200

    update SubscriptonsController.php

--- src/Controller/SubscriptionsController.php
+++ src/Controller/SubscriptionsController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class SubscriptionsController
+class SubscriptionsController extends AbstractController
 {
     public function list():Response
-    {
-        return new Response();   
+    {        
+        // return new Response();   
+  
+        $this->container;       // aus AbstractController
+
+        // $response = new Response();
+        // $response->setContent("<p>Subscriptions</p>");
+        // return $response;
+ 
+        $dataArray = [
+            'success' => true,
+            'subscriptions' => [
+                
+            ]
+        ];
+        return $this->json($dataArray);
     }
 }
